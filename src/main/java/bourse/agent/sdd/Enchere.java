2df6da88377835407a5e1ce825ce3ada9b4392ed
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2011-08-11 00:38:02 +0200

    Increase quality and unit testing of bourse.agent.sdd.Enchere.

--- src/main/java/bourse/agent/sdd/Enchere.java
+++ src/main/java/bourse/agent/sdd/Enchere.java
@@ -1,36 +1,74 @@
 package bourse.agent.sdd;
 
-import bourse.sdd.*;
+import bourse.sdd.Livre;
 
-/** Cette classe stocke les données relatives à une enchère. */
+/**
+ * Cette classe stocke les données relatives à une enchère.
+ */
 public class Enchere {
-    /**le duree restante avant la cloture de ce tour*/
+
+    /**
+     * la durée restante avant la cloture de ce tour
+     */
     private int temps;
-    /**le pas de l'enchere*/
+
+    /**
+     * le pas de l'enchere
+     */
     private float pas;
-    /**le nom de l'enchere*/
+
+    /**
+     * le nom de l'enchere
+     */
     private int type;
-    /**le nom de l'agent ayant effectué la plus grosse enchere, rien sinon*/
+
+    /**
+     * le nom de l'agent ayant effectué la plus grosse enchere, rien sinon
+     */
     private String nomagent;
-    /**numero representant le tour de l'enchere*/
+
+    /**
+     * numéro représentant le tour de l'enchère
+     */
     private int numeroEnchere;
-    /** Le livre qui concerne l'enchere. */
+
+    /**
+     * Le livre qui concerne l'enchère.
+     */
     private Livre livre;
-    /** La valeur de la derniere enchere, a deffaut le prix de depart. */
+
+    /**
+     * La valeur de la dernière enchere, à défaut le prix de départ.
+     */
     private float valeurEnchere;
-    /** Le prix maximun évalué par l'agent. */
+
+    /**
+     * Le prix maximun évalué par l'agent.
+     */
     private float prixMaximum = 0;
-        
-    /** Constructeurs. */
-    /** Construit une enchère entièrement définie. */
-    public Enchere( int num, Livre li, float val, int tps, float pas, int type, String nomag) {
-        this.numeroEnchere = num; this.livre = new Livre(li); this.valeurEnchere = val;
-        this.type = type; this.nomagent = nomag; this.pas = pas; this.temps = tps;
+
+    /**
+     * Construit une enchère entièrement définie.
+     */
+    public Enchere(int num, Livre li, float val, int tps, float pas, int type, String nomag) {
+        this.numeroEnchere = num;
+        this.livre = new Livre(li);
+        this.valeurEnchere = val;
+        this.type = type;
+        this.nomagent = nomag;
+        this.pas = pas;
+        this.temps = tps;
     }
-    /** Constructeur par défaut. */
+
+    /**
+     * Constructeur par défaut.
+     */
     public Enchere() {
-        numeroEnchere = 1; valeurEnchere = (float)0; livre = new Livre();
+        numeroEnchere = 1;
+        valeurEnchere = (float) 0;
+        livre = new Livre("", "", null, "", "", 0, 0, "", "", 0, "", 0);
     }
+
     public Enchere(bourse.agent.sdd.Enchere enchere) {
         this.numeroEnchere = enchere.getNumeroEnchere();
         this.livre = enchere.getLivre();
@@ -40,47 +78,76 @@ public class Enchere {
         this.temps = enchere.getTemps();
         this.pas = enchere.getPas();
     }
-    public int getTemps(){return this.temps;}
-    public int getType(){return this.type;}
-    public String getAgent(){return this.nomagent;}
-    public int getNumeroEnchere(){return this.numeroEnchere;}
-    public Livre getLivre(){return this.livre;}
-    public float getValeurEnchere(){return this.valeurEnchere;}
-    public float getPas(){return this.pas;}
-    public void setPrixMaximum(float _prixMaximum) { prixMaximum = _prixMaximum; }
-    public float getPrixMaximum() { return prixMaximum; }
-    /** Méthodes. */    
-    /** Méthode d'affichage. */
-    public String toString(int decalage) { 
-        String delta = "";
-        for (int i=0; i<decalage; i++) delta += " ";
-        return delta + "tour = " + this.numeroEnchere
-                   + ", type = " + this.type
-                   + ", temps = " + this.temps
-                   + ", pas = " + this.pas
-                   + ", enchérisseur = " + this.nomagent
-                   + ", prix = " + this.valeurEnchere
-                   + ", prix maximum = " + this.prixMaximum
-                   + ", livre = " + "\n" + this.livre.toString(decalage+1);
+
+    public int getTemps() {
+        return this.temps;
     }
+
+    public int getType() {
+        return this.type;
+    }
+
+    public String getAgent() {
+        return this.nomagent;
+    }
+
+    public int getNumeroEnchere() {
+        return this.numeroEnchere;
+    }
+
+    public Livre getLivre() {
+        return this.livre;
+    }
+
+    public float getValeurEnchere() {
+        return this.valeurEnchere;
+    }
+
+    public float getPas() {
+        return this.pas;
+    }
+
+    public void setPrixMaximum(float _prixMaximum) {
+        prixMaximum = _prixMaximum;
+    }
+
+    public float getPrixMaximum() {
+        return prixMaximum;
+    }
+
+    /**
+     * Méthode d'affichage.
+     */
+    public String toString(int decalage) {
+        final StringBuilder out = new StringBuilder();
+        for (int i = 0; i < decalage; i++) {
+            out.append(" ");
+        }
+        out.append("tour = ").append(numeroEnchere);
+        out.append(", type = ").append(type);
+        out.append(", temps = ").append(temps);
+        out.append(", pas = ").append(pas);
+        out.append(", enchérisseur = ").append(nomagent);
+        out.append(", prix = ").append(valeurEnchere);
+        out.append(", prix maximum = ").append(prixMaximum);
+        out.append(", livre = \n").append(livre.toString(decalage + 1));
+        return out.toString();
+    }
+
     public static int enchereToCode(String typeEnchere) {
-        if (typeEnchere.equalsIgnoreCase("EnchereUn")) return 1;
-        else if (typeEnchere.equalsIgnoreCase("EnchereDeux")) return 2;
-        else if (typeEnchere.equalsIgnoreCase("EnchereTrois")) return 3;
-        else if (typeEnchere.equalsIgnoreCase("EnchereQuatre")) return 4;
-        else if (typeEnchere.equalsIgnoreCase("EnchereCinq")) return 5;
-        else return 0;
+        if (typeEnchere.equalsIgnoreCase("EnchereUn")) {
+            return 1;
+        } else if (typeEnchere.equalsIgnoreCase("EnchereDeux")) {
+            return 2;
+        } else if (typeEnchere.equalsIgnoreCase("EnchereTrois")) {
+            return 3;
+        } else if (typeEnchere.equalsIgnoreCase("EnchereQuatre")) {
+            return 4;
+        } else if (typeEnchere.equalsIgnoreCase("EnchereCinq")) {
+            return 5;
+        } else {
+            return 0;
+        }
     }
-    /** La méthode main de test. */
-    public static void main(String[] argc) {
-        bourse.sdd.Livre l1 = new bourse.sdd.Livre("l1", "a2", new bourse.protocole.Categorie(), "poche", "O'reilly", (float)50.65, (float)0.45, "2004-01-01", "222222222", 1, "Seb", (float)65);
-        bourse.sdd.Livre l2 = new bourse.sdd.Livre("l2", "a1", new bourse.protocole.Categorie(), "poche", "Casterman", (float)40.75, (float)0.85, "1954-04-12", "222XX2254", 2, "protocoleman", (float)50);
-        bourse.sdd.Livre l3 = new bourse.sdd.Livre("l2", "a1", new bourse.protocole.Categorie(), "poche", "Casterman", (float)40.75, (float)0.27, "1954-04-12", "222XX2254", 3, "arno", (float)12);
-        Enchere e1 = new Enchere(1, l1, (float)19.81, 12, (float)1.2, 3, "a1");
-        Enchere e2 = new Enchere(2, l2, (float)20.00, 13, (float)1.2, 3, "a1");
-        Enchere e3 = new Enchere(3, l3, (float)10.01, 14, (float)1.2, 3, "a1");
-        System.out.println(e1.toString(4));
-        System.out.println(e2.toString(4));
-        System.out.println(e3.toString(4));
-    }
+
 }
---
+++ src/test/java/bourse/agent/sdd/EnchereTest.java
@@ -0,0 +1,35 @@
+package bourse.agent.sdd;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.junit.Test;
+
+import bourse.protocole.Categorie;
+import bourse.sdd.Livre;
+
+public class EnchereTest {
+
+    @Test
+    public final void instantiationAndToString() {
+        Livre l1 = new Livre("l1", "a2", new Categorie(), "poche", "O'reilly", 50.65f, 0.45f, "2004-01-01",
+                "222222222", 1, "Seb", 65f);
+        Livre l2 = new Livre("l2", "a1", new Categorie(), "poche", "Casterman", 40.75f, 0.85f, "1954-04-12",
+                "222XX2254", 2, "protocoleman", 50f);
+        Livre l3 = new Livre("l2", "a1", new Categorie(), "poche", "Casterman", 40.75f, 0.27f, "1954-04-12",
+                "222XX2254", 3, "arno", 12f);
+        Enchere e2 = new Enchere(2, l2, 20f, 13, 1.2f, 3, "a1");
+        Enchere e1 = new Enchere(1, l1, 19.81f, 12, 1.2f, 3, "a1");
+        Enchere e3 = new Enchere(3, l3, 10.01f, 14, 1.2f, 3, "a1");
+
+        assertThat(e1.toString(4))
+                .startsWith(
+                        "    tour = 1, type = 3, temps = 12, pas = 1.2, enchérisseur = a1, prix = 19.81, prix maximum = 0.0, livre = \n     Auteur = a2");
+        assertThat(e2.toString(4))
+                .startsWith(
+                        "    tour = 2, type = 3, temps = 13, pas = 1.2, enchérisseur = a1, prix = 20.0, prix maximum = 0.0, livre = \n     Auteur = a1");
+        assertThat(e3.toString(4))
+                .startsWith(
+                        "    tour = 3, type = 3, temps = 14, pas = 1.2, enchérisseur = a1, prix = 10.01, prix maximum = 0.0, livre = \n     Auteur = a1");
+    }
+
+}
