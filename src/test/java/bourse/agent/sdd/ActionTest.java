677b2629cf7087be3b6534f8dd4d3a395953d03c
Eric Le Merdy <eric.lemerdy@laposte.net>
2011-09-11 16:02:22 +0200

    Improve coverage 19.1% -> 20.3%

--- src/test/java/bourse/agent/sdd/ActionTest.java
+++ src/test/java/bourse/agent/sdd/ActionTest.java
@@ -1,23 +1,76 @@
 package bourse.agent.sdd;
 
+import static bourse.agent.sdd.Action.adversaires;
 import static bourse.agent.sdd.Action.attenteEnchere;
 import static bourse.agent.sdd.Action.aucune;
+import static bourse.agent.sdd.Action.bilan;
+import static bourse.agent.sdd.Action.migrer;
+import static bourse.agent.sdd.Action.objectif;
+import static bourse.agent.sdd.Action.programme;
 import static bourse.agent.sdd.Action.vendre;
 import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.verifyNew;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
+
+import java.util.Random;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+@RunWith(PowerMockRunner.class)
 public class ActionTest {
 
     @Test
-    public final void testNominal() {
-        Action action = new Action(aucune);
-        assertThat(action.getAction()).isEqualTo(aucune);
-        assertThat(action.toString(0)).isEqualTo("0 (aucune action séléctionnée)");
-        action = new Action(vendre);
-        assertThat(action.toString(0)).isEqualTo("1 (vendre son bouquin)");
-        action = new Action(attenteEnchere);
-        assertThat(action.toString(0)).isEqualTo("6 (attente d'une proposition enchère)");
+    @PrepareForTest(Action.class)
+    public void should_create_a_random_action() throws Exception {
+        Random random = mock(Random.class);
+        when(random.nextInt(6)).thenReturn(42);
+        whenNew(Random.class).withNoArguments().thenReturn(random);
+
+        Action action = new Action();
+
+        assertThat(action.getAction()).isEqualTo(42);
     }
 
+    @Test
+    public void should_print_action_name() {
+        assertThat(new Action(aucune).toString(8)).isEqualTo("        0 (aucune action séléctionnée)");
+        assertThat(new Action(vendre).toString(8)).isEqualTo("        1 (vendre son bouquin)");
+        assertThat(new Action(migrer).toString(8)).isEqualTo("        2 (migration)");
+        assertThat(new Action(bilan).toString(9)).isEqualTo("         3 (effectuer son bilan)");
+        assertThat(new Action(programme).toString(5)).isEqualTo("     4 (demande de programme)");
+        assertThat(new Action(adversaires).toString(3)).isEqualTo("   5 (demande de la liste des agents présents)");
+        assertThat(new Action(attenteEnchere).toString(0)).isEqualTo("6 (attente d'une proposition enchère)");
+        assertThat(new Action(objectif).toString(6)).isEqualTo("      7 ()");
+    }
+
+    @Test
+    public void should_set_action() {
+        Action action = new Action();
+
+        action.setAction(42);
+
+        assertThat(action.getAction()).isEqualTo(42);
+    }
+
+    @Test
+    @PrepareForTest(Action.class)
+    public void main_should_create_and_print_a_state() throws Exception {
+        Action action = mock(Action.class);
+        whenNew(Action.class).withNoArguments().thenReturn(action);
+        whenNew(Action.class).withArguments(anyInt()).thenReturn(action);
+
+        Action.main(null);
+
+        verifyNew(Action.class).withArguments(8);
+        verifyNew(Action.class, times(10)).withNoArguments();
+        verify(action, times(10)).toString(0);
+    }
 }
--- src/test/java/bourse/agent/sdd/EtatTest.java
+++ src/test/java/bourse/agent/sdd/EtatTest.java
@@ -1,9 +1,34 @@
 package bourse.agent.sdd;
 
+import static bourse.agent.sdd.Etat.actionChoisie;
+import static bourse.agent.sdd.Etat.attenteDeclenchementEnchere;
+import static bourse.agent.sdd.Etat.attentePropositionEnchere;
+import static bourse.agent.sdd.Etat.attenteRESULTATdeSaVente;
+import static bourse.agent.sdd.Etat.attenteRESULTBYE;
+import static bourse.agent.sdd.Etat.attenteRESULTWELCOME;
+import static bourse.agent.sdd.Etat.connaitPdms;
+import static bourse.agent.sdd.Etat.connectePhysiquement;
+import static bourse.agent.sdd.Etat.enchereDeuxOuCinq;
+import static bourse.agent.sdd.Etat.enchereInteressante;
+import static bourse.agent.sdd.Etat.enchereTrois;
+import static bourse.agent.sdd.Etat.enchereUnOuQuatre;
+import static bourse.agent.sdd.Etat.initial;
+import static bourse.agent.sdd.Etat.modeEnchere;
+import static bourse.agent.sdd.Etat.nonConnecte;
+import static bourse.agent.sdd.Etat.pdmChoisie;
+import static bourse.agent.sdd.Etat.pret;
+import static bourse.agent.sdd.Etat.quitter;
 import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+@RunWith(PowerMockRunner.class)
 public class EtatTest {
 
     @Test
@@ -36,8 +61,49 @@ public class EtatTest {
     }
 
     @Test
-    public void with_etat_2_should_print_status() {
-        Etat etat = new Etat(Etat.connaitPdms);
-        assertThat(etat.toString(3)).isEqualTo("   2 (connait les pdms actives)");
+    public void with_all_etat_should_print_status() {
+        assertThat(new Etat(quitter).toString(6)).isEqualTo("      0 (quitter)");
+        assertThat(new Etat(initial).toString(6)).isEqualTo("      1 (initialisé)");
+        assertThat(new Etat(connaitPdms).toString(2)).isEqualTo("  2 (connait les pdms actives)");
+        assertThat(new Etat(pdmChoisie).toString(3)).isEqualTo("   3 (pdm choisie dans la liste des actives)");
+        assertThat(new Etat(connectePhysiquement).toString(0)).isEqualTo("4 (connexion physique effectuée)");
+        assertThat(new Etat(attenteRESULTWELCOME).toString(0)).isEqualTo("5 (attente d'une réponse au WELCOME)");
+        assertThat(new Etat(pret).toString(9)).isEqualTo("         6 (pret pour travailler)");
+        assertThat(new Etat(attenteRESULTBYE).toString(4)).isEqualTo("    7 (attente d'une réponse au BYE)");
+        assertThat(new Etat(nonConnecte).toString(2)).isEqualTo("  8 (non connecté physiquement)");
+        assertThat(new Etat(actionChoisie).toString(0)).isEqualTo("9 (action choisie)");
+        assertThat(new Etat(attentePropositionEnchere).toString(0)).isEqualTo(
+                "10 (attente de réponse à la demande de vente)");
+        assertThat(new Etat(attenteDeclenchementEnchere).toString(0)).isEqualTo(
+                "11 (attente du déclenchement de la vente)");
+        assertThat(new Etat(modeEnchere).toString(1)).isEqualTo(" 12 (debut du mode enchère)");
+        assertThat(new Etat(attenteRESULTATdeSaVente).toString(0)).isEqualTo("13 (attente du résultat de sa vente)");
+        assertThat(new Etat(enchereInteressante).toString(0)).isEqualTo("14 (enchère interessante)");
+        assertThat(new Etat(enchereUnOuQuatre).toString(0)).isEqualTo(
+                "15 (enchère à prendre ou à laisser ou enchère descendante)");
+        assertThat(new Etat(enchereDeuxOuCinq).toString(0))
+                .isEqualTo("16 (enchère à pli scellé ou enchère de Vickrey)");
+        assertThat(new Etat(enchereTrois).toString(1)).isEqualTo(" 17 (enchère ascendante)");
+        assertThat(new Etat(42).toString(10)).isEqualTo("          42 (enchère non gérée)");
+    }
+
+    @Test
+    @PrepareForTest(Etat.class)
+    public void main_should_create_and_print_a_state() throws Exception {
+        Etat etat = mock(Etat.class);
+        whenNew(Etat.class).withArguments(2).thenReturn(etat);
+
+        Etat.main(null);
+
+        verify(etat).toString(0);
+    }
+
+    @Test
+    public void the_state_can_be_set() throws Exception {
+        Etat etat = new Etat(42);
+
+        etat.setEtat(24);
+
+        assertThat(etat.getEtat()).isEqualTo(24);
     }
 }
