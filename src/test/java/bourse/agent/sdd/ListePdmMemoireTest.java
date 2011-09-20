cf8e01c7eae74383e60367939807e765e55baf8d
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2011-09-20 22:16:32 +0200

    ListePdmMemoire is now fully test covered (except main() method)

--- src/test/java/bourse/agent/sdd/ListePdmMemoireTest.java
+++ src/test/java/bourse/agent/sdd/ListePdmMemoireTest.java
@@ -1,6 +1,10 @@
 package bourse.agent.sdd;
 
+import static com.google.common.collect.Iterators.forArray;
 import static org.fest.assertions.Assertions.assertThat;
+
+import java.util.Iterator;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,11 +18,10 @@ public class ListePdmMemoireTest {
     @Before
     public void initialisation() {
         Visualisation visu = new Visualisation();
-        visu.setVisible(true);
         listePdmMemoire = new ListePdmMemoire(visu);
         listePdmMemoire.ajouter(new PdmMemoire("p1", "HOME", false, false, new ListeProgramme(), 1));
         listePdmMemoire.ajouter(new PdmMemoire("p2", "192.168.1.2:80", true, true, new ListeProgramme(), 2));
-        listePdmMemoire.ajouter(new PdmMemoire("p3", "0.0.0.0:0", false, false, new ListeProgramme(), 2));
+        listePdmMemoire.ajouter(new PdmMemoire("p3", "0.0.0.0:0", false, true, new ListeProgramme(), 3));
     }
 
     @Test
@@ -27,10 +30,10 @@ public class ListePdmMemoireTest {
         assertThat(listePdmMemoire.acceder("p1").getVisitee()).isFalse();
         assertThat(listePdmMemoire.acceder("p2").getActive()).isTrue();
         assertThat(listePdmMemoire.acceder("p2").getVisitee()).isTrue();
-        assertThat(listePdmMemoire.acceder("p3").getActive()).isFalse();
+        assertThat(listePdmMemoire.acceder("p3").getActive()).isTrue();
         assertThat(listePdmMemoire.acceder("p3").getVisitee()).isFalse();
         assertThat(listePdmMemoire.aucuneActive()).isFalse();
-        assertThat(listePdmMemoire.premiereActiveNonVisitee()).isNull();
+        assertThat(listePdmMemoire.premiereActiveNonVisitee().getNom()).isEqualTo("p3");
         assertThat(listePdmMemoire.premiereActiveVisitee().getNom()).isEqualTo("p2");
 
         Pdm pdm1 = new Pdm("p1", "192.168.1.1:8080"); // p1 toujours active
@@ -66,5 +69,60 @@ public class ListePdmMemoireTest {
         assertThat(listePdmMemoire.acceder("p3").getVisitee()).isFalse();
         assertThat(listePdmMemoire.acceder("p4").getActive()).isTrue();
         assertThat(listePdmMemoire.acceder("p4").getVisitee()).isFalse();
+    }
+
+    @Test
+    public void when_removed_by_name_a_pdm_must_not_be_present_anymore() {
+        listePdmMemoire.ajouter(new PdmMemoire("pdmName", "127.0.0.1:1981", false, false, new ListeProgramme(), 3));
+
+        listePdmMemoire.supprimer(new PdmMemoire("pdmName", "192.168.0.1:8080", true, true, new ListeProgramme(), 76));
+
+        assertThat(listePdmMemoire.acceder("pdmName")).isNull();
+    }
+
+    @Test
+    public void new_pdm_list_must_be_empty() {
+        ListePdmMemoire pdmMemoryList = new ListePdmMemoire(null);
+        @SuppressWarnings("unchecked")
+        final Map<String, Pdm> internalList = pdmMemoryList.getListe();
+
+        assertThat(pdmMemoryList.estVide()).isTrue();
+        assertThat(internalList).isNotNull();
+        assertThat(internalList.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void pdm_list_must_be_transcoded_to_understandable_string() {
+        final ListePdmMemoire pdmList = new ListePdmMemoire(null);
+        final PdmMemoire newPdm = new PdmMemoire("pdmName", "127.0.0.1:1981", false, false, new ListeProgramme(), 1);
+        pdmList.ajouter(newPdm);
+
+        final String[] humanReadableString = pdmList.toString(1).split("\n");
+
+        Iterator<String> lines = forArray(humanReadableString);
+        assertThat(lines.next())
+                .isEqualTo(
+                        " nom = pdmName, adresse = [ip = 127.0.0.1, port = 1981], visitée = false, active = false, programme = ");
+        assertThat(lines.next()).startsWith("  date = ").endsWith(", liste = ");
+    }
+    
+    @Test
+    public void pdm_list_with_some_active_one_must_be_active() {
+        assertThat(listePdmMemoire.aucuneActive()).isFalse();
+    }
+    
+    @Test
+    public void pdm_list_with_no_active_ones_must_not_be_active() {
+        listePdmMemoire.supprimer(new PdmMemoire("p2", "127.0.0.1:1981", false, false, null, 0));
+        listePdmMemoire.supprimer(new PdmMemoire("p3", "127.0.0.1:1981", false, false, null, 0));
+        
+        assertThat(listePdmMemoire.aucuneActive()).isTrue();
+    }
+    
+    @Test
+    public void pdm_list_must_return_first_active_not_already_visited() {
+        final PdmMemoire firstActiveNotAlreadyVisited = listePdmMemoire.premiereActiveNonVisitee();
+        assertThat(firstActiveNotAlreadyVisited).isNotNull();
+        assertThat(firstActiveNotAlreadyVisited.getNom()).isEqualTo("p3");
     }
 }
