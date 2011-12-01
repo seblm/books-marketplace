43bef8642d071e7fe2982d48fb7206fa519b0d2c
Eric Le Merdy <eric.lemerdy@laposte.net>
2011-12-01 02:29:40 +0100

    Contributed a test in main code...

--- src/test/java/bourse/agent/sdd/ListeLivreTest.java
+++ src/test/java/bourse/agent/sdd/ListeLivreTest.java
@@ -1,106 +1,92 @@
 package bourse.agent.sdd;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.fest.assertions.MapAssert.entry;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.powermock.api.mockito.PowerMockito.verifyNew;
 import static org.powermock.api.mockito.PowerMockito.whenNew;
 
-import java.util.Collection;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import bourse.protocole.Categorie;
 import bourse.sdd.Livre;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(ListeLivre.class)
 public class ListeLivreTest {
 
-    private ListeLivre booksList;
-    private Livre book;
+    @Test
+    public void should_create_liste_livre() {
+        ListeLivre listeLivre = new ListeLivre();
 
-    @Before
-    public final void initNewAgentsList() {
-        booksList = new ListeLivre();
-        book = new Livre("title", "author", Categorie.newCategorieFromBd(Categorie.BD), "format", "editor", 10, 1,
-                "2010-01-01", "ISBN-NUM", 42, "owner", 9.3f);
-    }
-    
-    @Test
-    public final void should_not_keep_same_book_identified_by_id() {
-        booksList.ajouter(book);
-        final Livre bookWithId42 = new Livre("title", "author", Categorie.newCategorieFromBd(Categorie.BD), "format", "editor", 10, 1,
-                "2010-01-01", "ISBN-NUM", 42, "owner", 9.3f);
-        
-        booksList.ajouter(bookWithId42);
-        
-        assertThat(booksList.getValeurs()).hasSize(1).contains(bookWithId42).excludes(book);
-    }
-    
-    @Test
-    public final void should_removes_a_book_by_id() {
-        booksList.ajouter(book);
-        
-        booksList.supprimer(42);
-        
-        assertThat(booksList.getValeurs()).isEmpty();
+        assertThat(listeLivre.toString(3)).isEqualTo("");
     }
 
     @Test
-    public final void should_removes_a_book_by_instance() {
-        booksList.ajouter(book);
-        
-        booksList.supprimer(book);
-        
-        assertThat(booksList.getValeurs()).isEmpty();
+    public void should_add_and_retrieve_livre() {
+        ListeLivre listeLivre = new ListeLivre();
+        Livre livre = mock(Livre.class);
+
+        listeLivre.ajouter(livre);
+
+        assertThat(listeLivre.getValeurs()).containsOnly(livre);
+        assertThat(listeLivre.getListe()).includes(entry(0, livre));
     }
 
     @Test
-    public final void should_be_transformed_to_a_human_readable_string() {
-        booksList.ajouter(book);
+    public void with_non_empty_list_should_remove_livre_by_oder() {
+        ListeLivre listeLivre = new ListeLivre();
+        listeLivre.ajouter(mock(Livre.class));
 
-        final String stringRepresentation = booksList.toString(3);
+        listeLivre.supprimer(0);
 
-        assertThat(stringRepresentation).isEqualTo("       Auteur = author, Catégorie = Bandes Dessinées, Date Parution = 2010-01-01, Editeur = editor, Etat = 1.0, Format = format, Id = 42, Isbn = ISBN-NUM, Prix = 10.0, Titre = title, Proprietaire = owner, PrixAchat = 9.3");
+        assertThat(listeLivre.getValeurs()).isEmpty();
     }
 
     @Test
-    public final void empty_instance_should_be_transformed_to_empty_string() {
-        final String stringRepresentation = booksList.toString(0);
+    public void with_empty_list_should_not_remove_livre_by_oder() {
+        ListeLivre listeLivre = new ListeLivre();
 
-        assertThat(stringRepresentation).hasSize(0);
+        listeLivre.supprimer(0);
+
+        assertThat(listeLivre.getValeurs()).isEmpty();
     }
 
     @Test
-    public final void should_return_books_that_it_contains() {
-        booksList.ajouter(book);
-        final Livre secondBook = new Livre("title", "author", Categorie.newCategorieFromBd(Categorie.BD), "format", "editor", 10, 1,
-                "2010-01-01", "ISBN-NUM", 1, "owner", 9.3f);
-        booksList.ajouter(secondBook);
+    public void with_non_empty_list_should_remove_livre() {
+        ListeLivre listeLivre = new ListeLivre();
+        Livre livre = mock(Livre.class);
+        listeLivre.ajouter(livre);
 
-        @SuppressWarnings("rawtypes")
-        final Collection values = booksList.getValeurs();
+        listeLivre.supprimer(livre);
 
-        assertThat(values).containsOnly(book, secondBook);
+        assertThat(listeLivre.getValeurs()).isEmpty();
     }
 
     @Test
-    public final void should_be_executable() throws Exception {
-        final ListeLivre booksList = mock(ListeLivre.class);
-        whenNew(ListeLivre.class).withNoArguments().thenReturn(booksList);
+    public void with_empty_list_should_not_remove_livre() {
+        ListeLivre listeLivre = new ListeLivre();
+        Livre livre = mock(Livre.class);
 
-        ListeLivre.main(new String[0]);
+        listeLivre.supprimer(livre);
 
-        verifyNew(ListeLivre.class, times(1)).withNoArguments();
-        verify(booksList, times(3)).ajouter(any(Livre.class));
-        verify(booksList, times(1)).toString(5);
+        assertThat(listeLivre.getValeurs()).isEmpty();
     }
 
+    @Test
+    @PrepareForTest(ListeLivre.class)
+    public void should_launch_main_program() throws Exception {
+        ListeLivre listeLivre = spy(new ListeLivre());
+        whenNew(ListeLivre.class).withNoArguments().thenReturn(listeLivre);
+
+        ListeLivre.main(null);
+
+        verify(listeLivre, times(3)).ajouter(any(Livre.class));
+        verify(listeLivre).supprimer(any(Integer.class));
+        verify(listeLivre).supprimer(any(Livre.class));
+    }
 }
